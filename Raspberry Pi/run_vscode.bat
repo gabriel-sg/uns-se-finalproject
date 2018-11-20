03084062f344f82a0cbdec31332fc8896e431fc5
Gabriel SG <gsalazargisbert@gmail.com>
2018-11-20 17:23:51 -0300

    Archivos que sobran

--- Raspberry Pi/run_vscode.bat
+++
@@ -1,6 +0,0 @@
-:: runcode.cmd se encuentr en C:\Windows\System32
-:: Contiene el mismo codigo que C:\Program Files\Microsoft VS Code\bin\code.cmd
-:: pero con la diferencia que utiliza el comando start en vez de call
-:: para que se cierre la consola luego de ejecutar el comando
-
-runcode .
